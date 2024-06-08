c3fe9f921c4a421e7f726008be751242c18c73e4
oTrasel <trasel.lucas02@gmail.com>
2024-06-08 16:51:57 -0300

    ajustado o select das turmas

--- manager/selects/select_alunos_matriculados.php
+++ manager/selects/select_alunos_matriculados.php
@@ -2,19 +2,17 @@
 include('manager/conexao.php');
 
 try {
-    $stmt = $pdo->prepare("select distinct t.id, t.descr_turma from turma t 
+    $stmt = $pdo->prepare("select distinct t.id, t.descr_turma, n.id_vinculo  from turma t 
     join vinculo_turma_aluno vta 
     on vta.id_turma = t.id 
-    where t.dt_fechamento is null ");
+    left join nota n 
+    on n.id_vinculo = vta.id 
+    where t.dt_fechamento is null
+    and n.id_vinculo is null ");
     $stmt->execute();
     $row = $stmt->rowCount();
     $info_turma = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $aluno = $pdo->prepare("select a.id, a.nome  from aluno a 
-    where a.id not in (select vta.id_aluno from vinculo_turma_aluno vta) ");
-    $aluno->execute();
-    $row = $aluno->rowCount();
-    $info_aluno = $aluno->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $erro) {
     echo $erro;
 }
